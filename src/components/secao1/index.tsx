7ce3356e010ab7fc70ce8e9a112e231e5cbdfa4d
Alan silva <allansillva313@gmail.com>
2023-11-09 16:16:42 -0300

    first commit

---
+++ src/components/secao1/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Secao1() {
+  return (
+    <div className='flex flex-col gap-4 p-4 pt-24'>
+        <h3 className='text-3xl'>Olá, bem-vindo(a) a TechSolutions</h3>
+        <p className='text-lg'>Somos especializados em desenvolvimento de sites e lading pages para o seu negócio</p>
+
+        <div className='w-full flex items-center justify-center mt-10'>
+            <button className='bg-slate-600 p-4 rounded text-white'>Preciso de um site!</button>
+        </div>
+
+    </div>
+  )
+}
